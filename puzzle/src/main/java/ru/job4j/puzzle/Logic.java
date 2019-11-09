cdcec88ed7b86aac9fda866533130479113f6ca3
Oleg Kaschuk <okaschuk@gmail.com>
2019-11-09 19:05:39 +0400

    2. Сокобан. Тестовое задание.[#193808]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -71,31 +71,15 @@ public class Logic {
     public boolean isWin() {
         int[][] table = this.convert();
         boolean result = false;
-        for (int i = 0; i < table.length && !result; i++) {
-            if (table[i][i] == 1) {
-                result = isRowWin(table, i) || isColumnWin(table, i);
-            }
-        }
-        return result;
-    }
-
-    private static boolean isRowWin(int[][] board, int row) {
-        boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] != 1) {
-                result = false;
-                break;
-            }
-        }
-        return result;
-    }
-
-    private static boolean isColumnWin(int[][] board, int column) {
-        boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != 1) {
-                result = false;
-                break;
+        for (int index = 0; index != table.length && !result; index++) {
+            if (table[index][index] == 1) {
+                result = true;
+                for (int i = 0; i != table.length; i++) {
+                    if (table[index][i] != 1 && table[i][index] != 1) {
+                        result = false;
+                        break;
+                    }
+                }
             }
         }
         return result;
